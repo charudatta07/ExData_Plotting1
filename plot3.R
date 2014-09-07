7c36acce57da2e15c04980c0a01c491e9bb02239
charudatta07 <charudattadeshmukh@gmail.com>
2014-09-08 01:05:51 +0530

    Comitting Some Cosmetic Changes

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 ##Setting the working Directory
-setwd("C:/Users/Charudatta/Desktop/Career/Big Data Course/Koursera/Coursera-Exploratory Data Analysis-IV/exdata_data_household_power_consumption")
+setwd("C:/Users/Charudatta/exdata_data_household_power_consumption")
 ## Getting full dataset
 power <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
